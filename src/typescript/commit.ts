bab94a1a703b5c96303d78d137a6c889f91afc62
nb89portfolio <navraj.bains.89@outlook.com>
2025-04-13 13:51:11 -0400

    Testing commit refactor.

--- src/typescript/commit.ts
+++ src/typescript/commit.ts
@@ -16,22 +16,28 @@ async function getCommitMessage(): Promise<string> {
 }
 
 async function commit() {
-  const message = await getCommitMessage();
+  const readLine = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  exec(
-    `git add . && git commit -m "${message}" && git push origin dev"`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
+  readLine.question('Enter commit message: ', (message) => {
+    readLine.close();
+    exec(
+      `git add . && git commit -m "${message}" && git push origin dev"`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
       }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    }
-  );
+    );
+  });
 }
 
 commit();
