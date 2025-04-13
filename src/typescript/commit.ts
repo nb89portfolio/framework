487c57761ca8f858e92299cdb0010592ba8d2128
nb89portfolio <navraj.bains.89@outlook.com>
2025-04-13 13:53:00 -0400

    Test commit refactor.

--- src/typescript/commit.ts
+++ src/typescript/commit.ts
@@ -1,40 +1,27 @@
 import { exec } from 'child_process';
 import { createInterface } from 'readline';
 
-async function getCommitMessage(): Promise<string> {
-  const readLine = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) => {
-    readLine.question('Enter commit message: ', (message) => {
-      readLine.close();
-      resolve(message);
-    });
-  });
-}
-
 async function commit() {
-  const readLine = createInterface({
+  const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  readLine.question('Enter commit message: ', (message) => {
-    readLine.close();
+  rl.question('Enter commit message: ', (answer) => {
+    rl.close();
+
     exec(
-      `git add . && git commit -m "${message}" && git push origin dev"`,
+      `git add . && git commit -m "${answer}" && git push origin dev`,
       (error, stdout, stderr) => {
         if (error) {
-          console.error(`error: ${error.message}`);
+          console.error(`exec error: ${error.message}`);
           return;
         }
+
         if (stderr) {
           console.error(`stderr: ${stderr}`);
           return;
         }
-        console.log(`stdout: ${stdout}`);
       }
     );
   });
