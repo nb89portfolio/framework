a23a80b377a38edef6210f89f3be8e18e830a41c
nb89portfolio <navraj.bains.89@outlook.com>
2025-04-17 00:01:59 -0400

    Testing let extraction for dev commit.

--- src/typescript/commit.ts
+++ src/typescript/commit.ts
@@ -2,29 +2,39 @@ import { exec } from 'child_process';
 import { createInterface } from 'readline';
 
 async function commit() {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  try {
+    const readLine = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  rl.question('Enter commit message: ', (answer) => {
-    rl.close();
+    let test = '';
 
-    exec(
-      `git add . && git commit -m "${answer}" && git push origin dev`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error.message}`);
-          return;
+    readLine.question('Enter commit message: ', (answer) => {
+      readLine.close();
+
+      exec(
+        `git add . && git commit -m "${answer}" && git push origin dev`,
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error.message}`);
+            return;
+          }
+
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
         }
+      );
+    });
 
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return;
-        }
-      }
-    );
-  });
+    console.log(test);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log('Attempt to commit to dev branch is complete.');
+  }
 }
 
 commit();
